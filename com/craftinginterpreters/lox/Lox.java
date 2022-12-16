b6d553365f970d4d1c7cab6a167e5d5c7c2892f8
chenyulue <chenyulue@163.com>
2022-12-16 22:49:38 +0800

    07-Evaluating Expression finished

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -13,7 +13,9 @@ import static com.craftinginterpreters.lox.Token.*;
 
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -32,6 +34,7 @@ public class Lox {
 
         // Indicate an error in the exit code.
         if (hadError) { System.exit(65); }
+        if (hadRuntimeError) { System.exit(70); }
     }
 
     private static void runPrompt() throws IOException {
@@ -51,10 +54,12 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -72,5 +77,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + 
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
