5c088283e3a57e988f084ed58c6f45f7f6ad857a
chenyulue <chenyulue@163.com>
2023-01-13 22:26:22 +0800

    26-Garbage Collection/When to collect

--- lox/src/object.c
+++ lox/src/object.c
@@ -14,10 +14,16 @@
 static Obj *allocateObject(size_t size, ObjType type) {
     Obj *object = (Obj *)reallocate(NULL, 0, size);
     object->type = type;
+    object->isMarked = false;
 
     /* Add a new object node at the head of vm.objects. */
     object->next = vm.objects;
     vm.objects = object;
+
+#ifdef DEBUG_LOG_GC
+    printf("%p allocate %zu for %d\n", (void*)object, size, type);
+#endif
+
     return object;
 }
 
